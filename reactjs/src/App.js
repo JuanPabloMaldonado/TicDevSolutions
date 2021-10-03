90e086b39a9becce57ccd7a2bdc0791e90887fe1
beckan975 <mmanrique@misena.edu.co>
2021-10-03 12:51:01 -0500

    MM_Perfil y Registro de producto
    Se modifica el perfil y Registro de productos en react

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -6,6 +6,9 @@ import Login from './components/login/Login';
 import Registro from './components/registro/Registro';
 import Productos from './components/productos/Productos';
 import Roles from './components/roles/Roles';
+import Perfil from './components/perfil/Perfil';
+import Registroproducto from './components/registroproducto/Registroproducto';
+
 
 
 
@@ -16,9 +19,12 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
           <Route exact path="/registro" component={Registro} />
           <Route exact path="/productos" component={Productos} />
           <Route exact path="/roles" component={Roles} />
+          <Route exact path="/perfil" component={Perfil} />
+          <Route exact path="/registroproducto" component={Registroproducto} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ reactjs/src/components/perfil/Perfil.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router'
+import './perfil.css'
+import './styles.css'
+import './scripts.js'
+
+
+const Perfil = () => {
+
+
+    return (
+        <div className="perfilComponent">
+            <div className="bg-primary">
+                <div id="layoutAuthentication">
+                    <div id="layoutAuthentication_content">
+                        <main>
+                            <div className="container">
+                                <div className="row justify-content-center">
+                                    <div className="col-lg-7">
+
+                                        <div className="card shadow-lg border-0 rounded-lg mt-4">
+                                            <div className="card-header"><h3 className="text-center font-weight-light my-4">Actualizacion de Usuarios</h3></div>
+                                            <div className="card-body">
+                                                <form>
+
+                                                    <div className="form-floating mb-3">
+                                                        <label for="inputRol">ID</label>
+                                                        <select className="text-center form-control" type="text" placeholder="Ingrese su rol">
+                                                            <option disabled="disabled" selected="selected"></option>
+                                                            <option>0001</option>
+                                                            <option>0002</option>
+                                                            <option>0003</option>
+                                                            <option>0004</option>
+                                                            <option>0005</option>
+                                                            <option>0006</option>
+                                                            <option>0007</option>
+                                                            <option>0008</option>
+                                                            <option>0009</option>
+                                                            <option>0010</option>
+                                                            <option>0011</option>
+                                                            <option>0012</option>
+                                                            <option>0013</option>
+                                                            <option>0014</option>
+                                                            <option>0015</option>
+                                                            <option>0016</option>
+                                                            <option>0017</option>
+                                                            <option>0018</option>
+                                                            <option>0019</option>
+                                                            <option>0020</option>
+                                                            <option>0021</option>
+                                                            <option>0022</option>
+                                                            <option>0023</option>
+                                                        </select>
+                                                    </div>
+
+                                                    <div className="form-floating mb-3">
+                                                        <label for="inputRol">Rol</label>
+                                                        <select className="text-center form-control" type="text" placeholder="Ingrese su rol">
+                                                            <option disabled="disabled" selected="selected"></option>
+                                                            <option>Administrador</option>
+                                                            <option>Vendedor</option>
+                                                            <option>Usuario</option>
+                                                            <option>Proveedor</option>
+                                                        </select>
+                                                    </div>
+                                                    <div className="form-floating mb-3">
+                                                        <label for="inputStatus">Estado</label>
+                                                        <select className="text-center form-control" type="text" placeholder="Ingrese su rol">
+                                                            <option disabled="disabled" selected="selected"></option>
+                                                            <option>Activo</option>
+                                                            <option>Inactivo</option>
+
+                                                        </select>
+                                                    </div>
+
+                                                    <div className="mt-4 mb-0">
+                                                        <div className="d-grid"><a className="btn btn-primary btn-block" oneclick="sweetalertclick" href="#">Actualizar</a></div>
+                                                
+                                                    </div>
+                                                </form>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
+                    <div id="layoutAuthentication_footer">
+                        <footer className="py-4 bg-light mt-auto">
+                            <div className="container-fluid px-4">
+                                <div className="d-flex align-items-center justify-content-between small">
+                                    <div className="text-muted">Copyright &copy; TICDev's Solutions 2021 - Misión TIC 2022</div>
+                                    <div>
+                                        <a href="#">Política de privacidad</a>
+                                        &middot;
+                                        <a href="#">Términos &amp; Condiciones</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+                <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                <script src="/TICDevSolutions/Sprints/2/Main/sweetalert.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+                <script src="./scripts.js"></script>
+            </div>
+        </div >
+
+    )
+}
+
+
+export default Perfil
